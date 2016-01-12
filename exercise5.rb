a0a0831b396455801e573777258e7ed33ce76f3c
Bliss Man <blissman@gmail.com>
2016-01-12 15:39:11 -0500

    add full sentence string with conversion

--- exercise5.rb
+++ exercise5.rb
@@ -4,3 +4,5 @@ end
 
 puts "What temperature are you trying to convert?"
 temp_in_F = gets.chomp
+
+puts "The temperature of #{temp_in_F} degrees Fahrenheit is approx. #{FtoC(temp_in_F)} degrees Celsius."
