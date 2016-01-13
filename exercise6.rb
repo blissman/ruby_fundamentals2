115f5177849961bb33d3d01aab00faa8b47d958f
Bliss Man <blissman@gmail.com>
2016-01-12 20:20:39 -0500

    added check for bananas

--- exercise6.rb
+++ exercise6.rb
@@ -4,7 +4,14 @@ def display(array)
   }
 end
 
-grocery_list = ["carrots", "toilet paper", "apples", "salmon", "rice"]
+grocery_list = ["carrots", "bananas", "toilet paper", "apples", "salmon", "rice"]
 
 display(grocery_list)
+
 puts "total items: #{grocery_list.count}"
+
+if grocery_list.include?('bananas') == true
+  puts "You need to pick up bananas."
+else
+  puts "You don't need to pick up bananas today."
+end
