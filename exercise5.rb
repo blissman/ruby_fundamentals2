a7b95c1f3d0da5e0117593e8f09f0ffad19755c5
Bliss Man <blissman@gmail.com>
2016-01-13 10:51:19 -0500

    added comments to exercise5.rb for clarity

--- exercise5.rb
+++ exercise5.rb
@@ -1,8 +1,11 @@
+#Method for converting deg. F to deg. C
 def FtoC(f)
   return (f.to_i - 32) * 5/9
 end
 
+#gets input from user
 puts "What temperature are you trying to convert?"
 temp_in_F = gets.chomp
 
+#converts the temperatuer using the method FtoC(f)
 puts "The temperature of #{temp_in_F} degrees Fahrenheit is approx. #{FtoC(temp_in_F)} degrees Celsius."
