49e0c0a20ef7b0dbcef78531c3de97a5d77ff6f8
Bliss Man <blissman@gmail.com>
2016-01-12 20:53:57 -0500

    increased hash values by 5% and redisplayed

--- exercise7.rb
+++ exercise7.rb
@@ -15,3 +15,13 @@ students[:cohort4] = 43
 
 #Part 4: display name of all keys
 puts students.keys
+
+#Part 5: increase cohort sizes by 5% and redisplay the results
+#Increase by 5% and convert to integer (because there's no half-students)
+students.each {|cohort, num|
+  students[cohort] = (num*1.05).to_i
+}
+#display each value of updated hash
+students.each { |cohort, num|
+    puts cohort.to_s + ": " + num.to_s + " students"
+}
