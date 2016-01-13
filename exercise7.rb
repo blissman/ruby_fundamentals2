b5167fc175847d0af76eb1f3dad2bbfe1047923e
Bliss Man <blissman@gmail.com>
2016-01-12 20:46:16 -0500

    add cohort 4 to the list

--- exercise7.rb
+++ exercise7.rb
@@ -1,9 +1,14 @@
+#Part 1: create the hash.
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
   :cohort3 => 22
 }
 
+#Part 2: display the cohort: students.
 students.each { |cohort, num|
   puts cohort.to_s + ": " + num.to_s + " students"
 }
+
+#Part 3: add cohort 4.
+students[:cohort4] = 43
